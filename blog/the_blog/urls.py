7a0ce17bfe71f17c434516504aabcaeecf1730fc
AndreiKotM <andrew.m.kot@gmail.com>
2020-09-24 17:52:20 +0300

    4-d lesson

--- blog/the_blog/urls.py
+++ blog/the_blog/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import HomeView, ArticleDetailsView
+from .views import HomeView, ArticleDetailsView, AddPostView
 
 urlpatterns = [
-    # path('', views.home, name='home'),
     path('', HomeView.as_view(), name='home'),
     path('article/<int:pk>', ArticleDetailsView.as_view(), name='article details'),
+    path('add_post', AddPostView.as_view(), name='add post'),
 ]
