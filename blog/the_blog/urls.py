c672838ec8bda1ff7c9cf5730705ab01f90eea48
Andrei <andrei.m.kot@gmail.com>
2020-09-26 18:54:36 +0300

    13-th lesson

--- blog/the_blog/urls.py
+++ blog/the_blog/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import HomeView, ArticleDetailsView, AddPostView, UpdatePostView, DeletePostView
+from .views import *
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('article/<int:pk>', ArticleDetailsView.as_view(), name='article details'),
     path('add_post', AddPostView.as_view(), name='add post'),
+    path('add_category', AddCategoryView.as_view(), name='add category'),
     path('article/update/<int:pk>', UpdatePostView.as_view(), name='update post'),
     path('article/delete/<int:pk>', DeletePostView.as_view(), name='delete post'),
+    path('categories', CategoriesView.as_view(), name='categories'),
+    path('categories/<str:category_name>', get_posts_with_category, name='posts with category')
 ]
--- blog/the_blog/views.py
+++ blog/the_blog/views.py
@@ -1,12 +1,25 @@
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Post
-from .forms import PostForm, EditForm
+from .models import Post, Category
+from .forms import PostForm, EditForm, CategoryForm
 from django.urls import reverse_lazy
+from django.shortcuts import render
+
 
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
     ordering = ['creating_date']
+
+
+def get_posts_with_category(request, category_name):
+    posts_with_category = Post.objects.filter(category=category_name)
+    return render(request, 'posts_with_category.html',
+                  {'posts_with_category': posts_with_category, 'category_name': category_name})
+
+
+class CategoriesView(ListView):
+    model = Category
+    template_name = 'categories.html'
 
 
 class ArticleDetailsView(DetailView):
@@ -19,13 +32,20 @@ class AddPostView(CreateView):
     form_class = PostForm
     template_name = 'add_post.html'
 
+
+class AddCategoryView(CreateView):
+    model = Category
+    form_class = CategoryForm
+    template_name = 'add_category.html'
+
+
 class UpdatePostView(UpdateView):
     model = Post
     form_class = EditForm
     template_name = 'update_post.html'
 
+
 class DeletePostView(DeleteView):
     model = Post
     template_name = 'delete_post.html'
     success_url = reverse_lazy('home')
-
